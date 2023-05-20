0fb78152a6896cd4463e874223013edb63fea85a
zhaopenghao <henryzhao1989@163.com>
2023-05-20 10:56:36 +0800

    add dummy check

--- pdf2txt/utils.py
+++ pdf2txt/utils.py
@@ -6,13 +6,24 @@
 """
 from langchain.document_loaders import PyPDFLoader, PyMuPDFLoader, PDFMinerLoader
 
-def convert(src_file, dest_file):
+def convert(src_file, dest_file, column_num=1):
     """
     :param src_file: pdf
     :param dest_file: txt
     """
+
+    check_format(column_num)
+
     loader = PyMuPDFLoader(src_file)
     docs = loader.load()
     with open(dest_file, 'w') as f:
         for d in docs:
             f.write(d.page_content)
+
+
+def check_format(column_num):
+    # check if there's figure or not
+    # check if there's table or not
+    # check column_num
+    pass
+
